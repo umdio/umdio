17e33b170d988dcfc7edaa9444e68483e7d6a1fc
Evan McIntire <mcintire.evan@gmail.com>
2018-08-26 15:38:17 -0700

    Re-add pagination tests

--- tests/courses/pagination_spec.rb
+++ tests/courses/pagination_spec.rb
@@ -7,40 +7,40 @@ describe 'Pagination' do
   describe '/courses' do
 
     describe 'per_page' do
-      #before { get url }
-      #it 'should not be > 100' do
-        #get url + '?per_page=1000'
-        #res = JSON.parse(last_response.body)
-        #expect(res.length).to be 100
-      #end
+      before { get url }
+      it 'should not be > 100' do
+        get url + '?per_page=1000'
+        res = JSON.parse(last_response.body)
+        expect(res.length).to be 100
+      end
 
-      #it 'should always at least return 1 course' do
-        #get url + '?per_page=0&page=2'
-        #res = JSON.parse(last_response.body)
-        #expect(res.length).to be 1
-      #end
+      it 'should always at least return 1 course' do
+        get url + '?per_page=0&page=2'
+        res = JSON.parse(last_response.body)
+        expect(res.length).to be 1
+      end
 
-      #it 'should return the number of courses requested (between 1 and 100)' do
-        #num = 37
-        #get url + "?per_page=#{num}"
-        #res = JSON.parse(last_response.body)
-        #expect(res.length).to be num
-      #end
+      it 'should return the number of courses requested (between 1 and 100)' do
+        num = 37
+        get url + "?per_page=#{num}"
+        res = JSON.parse(last_response.body)
+        expect(res.length).to be num
+      end
     end
 
     describe 'response headers' do
-      #before { get url }
+      before { get url }
 
       # https://developer.github.com/v3/#link-header
-      #it 'should have a properly formatted Link' do
-        #expect(last_response.headers.has_key?('Link')).to be true
-        #match = last_response.headers['Link'].match(/<https?:\/\/[\S]+>; rel="(next|prev)"/)
-        #expect(match.nil?).to eq(false)
-      #end
+      it 'should have a properly formatted Link' do
+        expect(last_response.headers.has_key?('Link')).to be true
+        match = last_response.headers['Link'].match(/<https?:\/\/[\S]+>; rel="(next|prev)"/)
+        expect(match.nil?).to eq(false)
+      end
 
-      #it 'should have X-Total-Count' do
-        #expect(last_response.headers.has_key?('X-Total-Count')).to be true
-      #end
+      it 'should have X-Total-Count' do
+        expect(last_response.headers.has_key?('X-Total-Count')).to be true
+      end
     end
   end
 end
