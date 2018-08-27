9fb27f549df1923e77f3eb61b2f352a65eeca6ed
Evan McIntire <mcintire.evan@gmail.com>
2018-08-27 16:13:17 -0700

    Remove dead commented code, consolidate test spec helpers

--- server.rb
+++ server.rb
@@ -1,6 +1,6 @@
 # umdio api core application. brings in other dependencies as needed.
 ENV['RACK_ENV'] ||= 'development'
- 
+
 require 'bundler'
 Bundler.require :default, ENV['RACK_ENV'].to_sym
 
@@ -18,26 +18,15 @@ class UMDIO < Sinatra::Base
   # Explicitly set this as the root file
   set :root, File.dirname(__FILE__)
 
-  # fix strange scraper bug by explicitly setting the server
-  # reference: http://stackoverflow.com/questions/17334734/how-do-i-get-sinatra-to-work-with-httpclient
-  
-  
   configure do
     # set up mongo database - code from ruby mongo driver tutorial
     host = ENV['MONGO_RUBY_DRIVER_HOST'] || 'localhost'
     port = ENV['MONGO_RUBY_DRIVER_PORT'] || MongoClient::DEFAULT_PORT
     puts "Connecting to mongo on #{host}:#{port}"
     # we might need other databases for other endpoints, but for now this is fine, with multiple collections
-    set :courses_db, MongoClient.new(host, port, pool_size: 20, pool_timeout: 5).db('umdclass') 
+    set :courses_db, MongoClient.new(host, port, pool_size: 20, pool_timeout: 5).db('umdclass')
     set :buses_db, MongoClient.new(host,port, pool_size: 20, pool_timeout: 5).db('umdbus')
     set :map_db, MongoClient.new(host,port, pool_size: 20, pool_timeout: 5).db('umdmap')
-    #set :profs_db, MongoClient.new(host, port, pool_size: 20, pool_timeout: 5).db('umdprof')
-  end
-
-  configure :development do
-    # TODO: fix weird namespace conflict and install better_errors
-    use BetterErrors::Middleware
-    BetterErrors.application_root = __dir__
   end
 
   # before application/request starts
--- tests/professors_spec.rb
+++ tests/professors_spec.rb
@@ -3,14 +3,6 @@ require_relative '../tests/spec_helper'
 describe 'Professors Endpoint' do
   url = "v0/professors"
 
-  shared_examples_for 'good status' do |url|
-    before {get url}
-    it 'has a good response' do
-      expect(last_response.status).to be == 200
-      expect(last_response.body.length).to be > 1
-    end
-  end
-
   describe 'get /professors' do
     it_has_behavior 'good status', url
   end
