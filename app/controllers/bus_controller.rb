7360535b2bbf70e7a6e26f23fb4cd12fcabdec87
Evan McIntire <mcintire.evan@gmail.com>
2019-10-31 19:31:40 -0400

    Fix bus errors

--- app/controllers/bus_controller.rb
+++ app/controllers/bus_controller.rb
@@ -4,11 +4,11 @@ module Sinatra
   module UMDIO
     module Routing
       module Bus
-
         def self.registered(app)
           #this should probably be a more specific error message where we error out!
           bad_route_message = "umd.io doesn't know the bus route in your url. Full list at https://api.umd.io/v0/bus/routes"
           bad_stop_message = "umd.io doesn't know the stop in your url. Full list at https://api.umd.io/v0/bus/routes"
+          bus_docs_url = "https://docs.umd.io/bus"
           apiRoot = 'http://webservices.nextbus.com/service/publicJSONFeed?a=umd'
           require 'net/http'
 
@@ -31,7 +31,7 @@ module Sinatra
           # in nextbus api terms, this is routeConfig - the info for a route
           app.get '/v0/bus/routes/:route_id' do
             route_ids = params[:route_id].downcase.split(",")
-            route_ids.each {|route_id| halt 400, bad_url_error(bad_route_message) unless is_route_id? route_id}
+            route_ids.each {|route_id| halt 400, bad_url_error(bad_route_message, bus_docs_url) unless is_route_id? route_id}
             routes = Route.where(route_id: route_ids).map {|r| r.to_v0}
             # get rid of [] on single object return
             routes = routes[0] if route_ids.length == 1
@@ -48,7 +48,7 @@ module Sinatra
             cache_control :public, :must_revalidate, max_age: 60*60
 
             route_id = params[:route_id]
-            halt 400, bad_url_error(bad_route_message) unless is_route_id? route_id
+            halt 400, bad_url_error(bad_route_message, bus_docs_url) unless is_route_id? route_id
             # address = apiRoot + "&command=schedule"
             # Net::HTTP.get(URI(address + "&r=#{route_id}")).to_s
             #json schedules_collection.find({route: route_id},{fields:{_id:0,schedule_class:0}}).to_a
@@ -62,8 +62,8 @@ module Sinatra
 
             route_id = params[:route_id]
             stop_id = params[:stop_id]
-            halt 400, bad_url_error(bad_route_message)  unless is_route_id? route_id
-            halt 400, bad_url_error(bad_stop_message) unless is_stop_id? stop_id
+            halt 400, bad_url_error(bad_route_message, bus_docs_url)  unless is_route_id? route_id
+            halt 400, bad_url_error(bad_stop_message, bus_docs_url) unless is_stop_id? stop_id
             address  = apiRoot + "&command=predictions"
             Net::HTTP.get(URI(address + "&r=#{route_id}&s=#{stop_id}")).to_s #this weirdness is from nextbus's api. I swear.
           end
@@ -73,7 +73,7 @@ module Sinatra
             cache_control :public, :must_revalidate, :no_cache, max_age: 60
 
             route_id = params[:route_id]
-            halt 400, bad_url_error(bad_route_message) unless is_route_id? route_id
+            halt 400, bad_url_error(bad_route_message, bus_docs_url) unless is_route_id? route_id
             address = apiRoot + "&command=vehicleLocations"
             Net::HTTP.get(URI(address + "&r=#{route_id}")).to_s
           end
@@ -94,7 +94,7 @@ module Sinatra
           # get info about a particular bus stop
           app.get '/v0/bus/stops/:stop_id' do
             stop_id = params[:stop_id]
-            halt 400, bad_url_error(bad_stop_message) unless is_stop_id? stop_id
+            halt 400, bad_url_error(bad_stop_message, bus_docs_url) unless is_stop_id? stop_id
             json Stop.where(stop_id: stop_id).map {|s| s.to_v0}
           end
 
--- app/helpers/errors.rb
+++ app/helpers/errors.rb
@@ -9,16 +9,17 @@ module Sinatra
           message: message,
           docs: docs
         }.to_json
-    end
+      end
 
-    def not_find_error message, docs
-        message ||= "Check your url! It doesn't seem to correspond to anything on the umd.io api. If you think it should, create an issue on our github page."
-        docs ||= "https://docs.umd.io"
-        {
-          error_code: 404,
-          message: message,
-          docs: docs
-        }.to_json
+      def not_find_error message, docs
+          message ||= "Check your url! It doesn't seem to correspond to anything on the umd.io api. If you think it should, create an issue on our github page."
+          docs ||= "https://docs.umd.io"
+          {
+            error_code: 404,
+            message: message,
+            docs: docs
+          }.to_json
+      end
     end
   end
 end
