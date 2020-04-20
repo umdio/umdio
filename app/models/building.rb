e000815f606d5c0e3f5cbe7921a44713c0f1d278
Evan McIntire <mcintire.evan@gmail.com>
2020-04-20 17:07:14 -0400

    Consistent indenting

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,30 +1,30 @@
 $DB.create_table? :buildings do
-    primary_key :pid
-    String :name
-    String :code
-    String :id
-    Float :long
-    Float :lat
+  primary_key :pid
+  String :name
+  String :code
+  String :id
+  Float :long
+  Float :lat
 end
 
 class Building < Sequel::Model
-    def to_v0
-        {
-            name: name,
-            code: code,
-            id: id,
-            lng: long.to_s,
-            lat: lat.to_s
-        }
-    end
+  def to_v0
+    {
+      name: name,
+      code: code,
+      id: id,
+      lng: long.to_s,
+      lat: lat.to_s
+    }
+  end
 
-    def to_v1
-        {
-            name: name,
-            code: code,
-            id: id,
-            long: long,
-            lat: lat
-        }
-    end
+  def to_v1
+    {
+      name: name,
+      code: code,
+      id: id,
+      long: long,
+      lat: lat
+    }
+  end
 end
--- app/models/majors.rb
+++ app/models/majors.rb
@@ -1,27 +1,27 @@
 $DB.create_table? :majors do
-    primary_key :pid
-    String :major_id
-    String :name
-    String :college
-    String :url
+  primary_key :pid
+  String :major_id
+  String :name
+  String :college
+  String :url
 end
 
 class Major < Sequel::Model
-    def to_v1
-        {
-            major_id: pid,
-            name: name,
-            college: college,
-            url: url
-        }
-    end
+  def to_v1
+    {
+      major_id: pid,
+      name: name,
+      college: college,
+      url: url
+    }
+  end
 
-    def to_v0
-        {
-            major_id: major_id,
-            name: name,
-            college: college,
-            url: url
-        }
-    end
+  def to_v0
+    {
+      major_id: major_id,
+      name: name,
+      college: college,
+      url: url
+    }
+  end
 end
