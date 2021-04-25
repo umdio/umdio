141d411dab5177caf7f92defba267966bd7a621c
Don Isaac <donald.isaac@gmail.com>
2021-04-25 17:31:03 -0400

    Set CI flag in workflow, spec_helper checks for it. Also some workflow bug fixes

--- tests/spec_helper.rb
+++ tests/spec_helper.rb
@@ -1,8 +1,10 @@
 require 'simplecov'
 SimpleCov.start
 
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 require 'json'
 
