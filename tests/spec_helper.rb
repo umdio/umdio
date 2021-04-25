9430708fe31740f3d77468821d3470fcb879e0ab
Don Isaac <donald.isaac@gmail.com>
2021-04-25 17:22:56 -0400

    Added docker layer caching, removed arbitrary sleep command

--- tests/spec_helper.rb
+++ tests/spec_helper.rb
@@ -1,10 +1,9 @@
 require 'simplecov'
 SimpleCov.start
 
-if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 require 'json'
 
 ENV['RACK_ENV'] = 'test'
