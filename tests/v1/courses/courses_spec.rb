5c7e5349d38dfd3c7438d1fb5ca0a7bf2e420314
Don Isaac <donald.isaac@gmail.com>
2021-04-25 21:28:30 -0400

    Added a bunch of tests for courses endpoints

--- tests/v1/courses/courses_spec.rb
+++ tests/v1/courses/courses_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../../spec_helper.rb'
+require_relative '../../spec_helper'
 
 def build_url1(u)
   "/v1/courses#{u}semester=201808"
@@ -105,6 +105,86 @@ describe 'Courses Endpoint v1', :endpoint, :courses do
       it_has_behavior 'bad status', (build_url1 '/sections/enes10-0101?')
       it_has_behavior 'bad status', (build_url1 '/sections/ene100-0101?')
       it_has_behavior 'bad status', (build_url1 '/sections/enes100-0101,enes102-010?')
+    end
+  end
+
+  describe 'GET /courses/sections' do
+    url = '/v1/courses/sections'
+
+    describe 'with no queries' do
+      before do
+        get url
+        @res = JSON.parse(last_response.body)
+      end
+
+      it_has_behavior 'good status', url
+
+      it 'returns an array of courses' do
+        pending
+        expect(@res).to be_a_kind_of Array
+        expect(@res).to all include(
+          semester: (a_kind_of String),
+          number: (a_kind_of String),
+          seats: (a_kind_of String),
+          open_seats: (a_kind_of String),
+          waitlist: (a_kind_of String),
+          instructors: (all a_kind_of String),
+          meetings: (a_kind_of(Array) & (all include(
+            days: (a_kind_of String),
+            room: (a_kind_of String),
+            building: (a_kind_of String),
+            classtype: (a_kind_of String),
+            end_time: (a_kind_of String)
+          )))
+        )
+      end
+    end
+
+    describe 'with query params' do
+      context 'per_page' do
+        # TODO(don): Should this return 400?
+        it_has_behavior 'good status', (url + '?per_page=-5')
+        it_has_behavior 'good status', (url + '?per_page=100')
+        it_has_behavior 'good status', (url + '?per_page=200')
+
+        context 'elements per page' do
+          context 'returns n elements when n <= 100, 100 otherwise' do
+            it 'n = 0' do
+              pending
+              get("#{url}?per_page=0")
+              res = JSON.parse(last_response.body)
+              expect(res.length).to eq 0
+            end
+
+            it 'n = 1' do
+              get("#{url}?per_page=1")
+              res = JSON.parse(last_response.body)
+              expect(res.length).to eq 1
+            end
+
+            it 'n = 50' do
+              pending 'am I doing this wrong'
+              get("#{url}?per_page=50")
+              res = JSON.parse(last_response.body)
+              expect(res.length).to eq 50
+            end
+
+            it 'n = 100' do
+              pending 'am I doing this wrong'
+              get("#{url}?per_page=100")
+              res = JSON.parse(last_response.body)
+              expect(res.length).to eq 100
+            end
+
+            it 'n = 200' do
+              pending 'am I doing this wrong'
+              get("#{url}?per_page=200")
+              res = JSON.parse(last_response.body)
+              expect(res.length).to eq 100
+            end
+          end
+        end
+      end
     end
   end
 end
