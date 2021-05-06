d4d779b74469b062aa571a9e1feb60f48613f945
Don Isaac <donald.isaac@gmail.com>
2021-05-05 21:56:52 -0400

    Added v1 map shape test

--- tests/v1/map_spec.rb
+++ tests/v1/map_spec.rb
@@ -8,7 +8,24 @@ describe 'Map Endpoints v1', :endpoint, :map do
   end
 
   describe 'get /map/buildings' do
-    it_has_behavior 'good status', url + '/buildings'
+    let(:res) { JSON.parse(last_response.body) }
+
+    include_examples 'good status', url + '/buildings'
+
+    it 'returns a nonempty array' do
+      expect(res).to be_an Array
+      expect(res).not_to be_empty
+    end
+
+    it 'returns a list of buildings' do
+      expect(res).to all include(
+        'name' => (a_kind_of String),
+        'code' => (a_kind_of String),
+        'id' => (a_kind_of String),
+        'lat' => (a_kind_of Float),
+        'long' => (a_kind_of Float)
+      )
+    end
   end
 
   describe 'get /map/buildings/:building_id' do
