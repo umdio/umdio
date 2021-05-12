049c3b41301aaa998a5710214d95a96d40212461
Don Isaac <donald.isaac@gmail.com>
2021-05-12 19:20:45 -0400

    Added fields to Buildings - Added state, city, street, street name, and address columns - Added data for waaayyyyy more buildings

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,8 +1,16 @@
 $DB.create_table? :buildings do
-  primary_key :pid
-  String :name
-  String :code
-  String :id
+  Integer :id, primary_key: true
+  String :name # Full building name
+  String :code, unique: true, nullable: true # Building name, if applicable. E.G. "AVW"
+
+  # Location and address data
+  String :city
+  String :state
+  Integer :zip
+  String :street       # Full address, e.g. 8125 Paint Branch Dr.
+  String :street_name  # Just the street name, e.g. Paint Branch
+  String :address_num  # Just the address number, e.g. 8125
+
   Float :long
   Float :lat
 end
@@ -12,7 +20,7 @@ class Building < Sequel::Model
     {
       name: name,
       code: code,
-      id: id,
+      id: id.to_s,
       lng: long.to_s,
       lat: lat.to_s
     }
@@ -23,6 +31,12 @@ class Building < Sequel::Model
       name: name,
       code: code,
       id: id,
+      city: city,
+      state: state,
+      zip: zip,
+      street: street,
+      street_name: street_name,
+      address_num: address_num,
       long: long,
       lat: lat
     }
