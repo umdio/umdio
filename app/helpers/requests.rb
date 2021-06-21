e272bee7ff71160fe43a3e0c162d800130587f5b
Liam DeVoe <orionldevoe@gmail.com>
2021-06-21 10:56:53 -0400

    touch up bus request code

--- app/helpers/requests.rb
+++ app/helpers/requests.rb
@@ -6,11 +6,6 @@ module Sinatra
       def wrapRequest(url)
         Net::HTTP.get(URI(url + '&t=0')).to_s
       end
-
-      def wrapRequest_v1(url)
-        Net::HTTP.get(URI(url + '&t=0')).to_s
-        halt 502, 'Bus Service Unavailible'
-      end
     end
   end
 end
