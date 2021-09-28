bd9e91a6a9649fda3ca35617ff37586359dfe2ca
Don Isaac <donald.isaac@gmail.com>
2021-09-28 13:57:21 -0400

    Wrote bus tests to prepare for upcoming UMO IQ migration

--- tests/matchers/bus_matcher.rb
+++ tests/matchers/bus_matcher.rb
@@ -5,4 +5,10 @@ module BusMatchers
   matcher :be_a_bus_route do
     match { |actual| actual.is_a?(Hash) and actual['route_id'].is_a?(String) and actual['title'].is_a?(String) }
   end
+
+  matcher :be_populated do
+    match { |actual| actual.respond_to?(:empty?) && !actual.empty? }
+  end
+
+  alias_matcher :populated, :be_populated
 end
